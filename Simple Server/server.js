7a7a5fcde7053da364c16a77bedf731a92cd461c
Tyler Nielson <tyler.snielson@gmail.com>
2018-02-08 14:03:04 -0600

    added handler for edge case of root dir. not sure it'll work but it's something.

--- Simple Server/server.js
+++ Simple Server/server.js
@@ -8,6 +8,40 @@ const path = require('path');
 // Port definition
 const PORT = 8008;
 
+/**
+ * @function serveRoot
+ * Serves the root directory since there is a weird issue with trying to link subdirectories vs. the root directory.
+ * @param {string} filePath The path to the directory.
+ * @param {http.ClientRequest} req The http request object.
+ * @param {http.ServerResponse} res The http response object.
+ */
+function serveRoot(filePath, req, res) {
+    console.log("serving the root directory");
+    fs.readdir(filePath, function(err, files) {
+        if(err) {
+            console.error(err);
+            res.statusCode = 500;
+            res.end("Server ERror: Could not read file.");
+        }
+        var html = "<p>Index of " + filePath + "</p>";
+        html += "<ul>";
+        html += files.map(function(item) {
+            console.log("<li><a href='" + item + "'>" + item + "</a></li>");
+            return "<li><a href='" + item + "'>" + item + "</a></li>";
+        }).join("");
+        html += "</ul>";
+        res.end(html);
+    });
+}
+
+/**
+ * @function serveIndex
+ * Determines if the directory contains an index.html file. If it does, the server serves the index file. otherwise it checks for the root
+ * directory and serves that if the directory is the root directory, or serves the subdirectory.
+ * @param {string} filePath The path to the directory.
+ * @param {http.ClientRequest} req The http request object.
+ * @param {http.ServerResponse} res The http response object.
+ */
 function serveIndex(filePath, req, res) {
     console.log(`${filePath} is a directory.`);
 
@@ -28,17 +62,28 @@ function serveIndex(filePath, req, res) {
             }
         }
         if (!indexExists) {
-            var html = "<p>Index of " + filePath + "</p>";
-            html += "<ul>";
-            html += files.map(function(item) {
-                return "<li><a href='" + req.url + '/' + item + "'>" + item + "</a></li>";
-            }).join("");
-            html += "</ul>";
-            res.end(html);
+            if (req.url === '/') {
+                serveRoot(filePath, req, res);
+            } else {
+                console.log(`Serving the ${req.url} directory.`);
+                var html = "<p>Index of " + filePath + "</p>";
+                html += "<ul>";
+                html += files.map(function(item) {
+                    console.log("<li><a href='" + req.url + '/' + item + "'>" + item + "</a></li>");
+                    return "<li><a href='" + req.url + '/' + item + "'>" + item + "</a></li>";
+                }).join("");
+                html += "</ul>";
+                res.end(html);
+            }
         }
     });
 }
 
+/** @function serveFile
+ * Serves the specified file with the provided response object
+ * @param {string} filePath Specifies the file path to read
+ * @param {http.serverResponse} res the http response object
+ */
 function serveFile(filePath, res) {
     console.log(`${filePath} is a file.`);
     fs.readFile(filePath, function(err, data) {
@@ -55,6 +100,12 @@ function serveFile(filePath, res) {
     });
 }
 
+/**
+ * @function setTheHeader
+ * Returns the correct header type based on the provided file extension.
+ * @param {string} extension The extension of the file that is being requested
+ * @returns {string} The header type of the file. 
+ */
 function setTheHeader(extension) {
     switch(extension) {
         case '.html':
@@ -82,6 +133,12 @@ function setTheHeader(extension) {
     }
 }
 
+/**
+ * @function handleRequest
+ * Request handler for the server
+ * @param {http.ClientRequest} req The http client request object 
+ * @param {http.ServerResponse} res The http server response object
+ */
 function handleRequest(req, res) {
     // This is a pretty general "catch-all" for any error that may occur when determining if a file exists. I believe this is sufficient enough for this assignment.
     // The following checks if the path is a directory, then a file, then if the file even exists and handles each possibility as such.
@@ -110,8 +167,10 @@ function handleRequest(req, res) {
     });
 }
 
+// Create the Server
 var server = http.createServer(handleRequest);
 
+// Start the server
 server.listen(PORT, function() {
     console.log(`Listening on port ${PORT}`);
 });
